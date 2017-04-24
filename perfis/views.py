bb2453c0e23549b7135ddef35127c995e5357f98
Alvaro <alvaro.camilloneto@gmail.com>
2017-04-24 00:04:26 -0300

    Correção da chamada da View

--- perfis/views.py
+++ perfis/views.py
@@ -5,5 +5,5 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     """Pagina Inicial"""
-    return HttpResponse('Bem Vindo ao Connectedin')
+    return render(request, 'perfis\index.html')
 
