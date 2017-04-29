f9a28189ac4d85ec2cee51861b5f9d8d7f45c5de
Alvaro <alvaro.camilloneto@gmail.com>
2017-04-29 00:00:54 -0300

    Incluida a Funcionalidade de convidar Amigo

--- perfis/urls.py
+++ perfis/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from perfis.views import index, exibir
+from perfis.views import index, exibir, convidar
 
 urlpatterns = [
     url(r'^$', index, name='index'),
-    url(r'^perfis/(?P<perfil_id>\d+)/$', exibir, name='perfis'),
+    url(r'^perfis/(?P<perfil_id>\d+)/$', exibir, name='exibir'),
+    url(r'^perfis/(?P<perfil_id>\d+)/convidar$', convidar, name='convidar'),
 ]
--- perfis/views.py
+++ perfis/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from perfis.models import Perfil
+from django.shortcuts import redirect
 
 
 # Create your views here.
 def index(request):
     """Pagina Inicial"""
-    perfis = Perfil.objects.all()
-    return render(request, 'perfis\index.html', {'perfis': perfis})
+    return render(request, 'perfis\index.html', {'perfis': Perfil.objects.all()})
 
 
 def exibir(request, perfil_id):
@@ -14,3 +14,15 @@ def exibir(request, perfil_id):
 
     perfil = Perfil.objects.get(id=perfil_id)
     return render(request, 'perfis\perfil.html', {"perfil": perfil})
+
+
+def convidar(request, perfil_id):
+    """View da pagina de Perfil"""
+    perfil_convidar = Perfil.objects.get(id=perfil_id)
+    perfil_logado = get_perfil_logado(request)
+    perfil_logado.convidar(perfil_convidar)
+    return redirect('index')
+
+
+def get_perfil_logado(request):
+    return Perfil.objects.get(id='1')
