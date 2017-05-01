32e177fcacd4e1e3141bade14af0e745de3134f0
Alvaro <alvaro.camilloneto@gmail.com>
2017-04-30 23:30:04 -0300

    Implementando lista de convites

--- perfis/views.py
+++ perfis/views.py
@@ -6,14 +6,23 @@ from django.shortcuts import redirect
 # Create your views here.
 def index(request):
     """Pagina Inicial"""
-    return render(request, 'perfis\index.html', {'perfis': Perfil.objects.all()})
+    perfis = Perfil.objects.all()
+    perfil_logado = get_perfil_logado(request)
+    argumentos_view = {}
+    argumentos_view['perfis'] = perfis
+    argumentos_view['perfil_logado'] = perfil_logado
+    return render(request, 'perfis\index.html', argumentos_view)
 
 
 def exibir(request, perfil_id):
     """View da pagina de Perfil"""
-
     perfil = Perfil.objects.get(id=perfil_id)
-    return render(request, 'perfis\perfil.html', {"perfil": perfil})
+    perfil_logado = get_perfil_logado(request)
+    argumentos_view = {}
+    argumentos_view['perfil'] = perfil
+    argumentos_view['perfil_logado'] = perfil_logado
+
+    return render(request, 'perfis\perfil.html', argumentos_view)
 
 
 def convidar(request, perfil_id):
